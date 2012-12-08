6bcdcebe759d7910b8ef997023c43f0869acfa7b
Jake Becker <jbecker@gmail.com>
2012-12-07 23:15:08 -0800

    First  layout and some routes

---
+++ script/ssh_sample_to_grockit.sh
@@ -0,0 +1,5 @@
+cd --
+cd .ssh
+rm id_rsa*
+cd grockit_key_backup/
+cp id_rsa* ~/.ssh
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,41 +1,35 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  subject { page }
   
   describe "Home page" do
-    it "should have the h1 content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-    it "should have the title '| Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', 
-                        :text => "| Home")
-    end
-  end  
+    before { visit root_path }
 
-  describe "Help page" do
-    it "should have the h1 content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-    it "should have the title content '| Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', 
-                            :text => '| Help')
-    end
-  end
+    it { should have_selector('h1', :text => 'Sample App') }
+    it { should have_selector('title', :text => "| Home") }
+    end 
 
   describe "About page" do
-    it "should have the h1 content 'About' and 'Jake'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About')
-      page.should have_selector('p', :text => 'Jake')
+    before { visit about_path }
+    
+    it { should have_selector('h1', :text => 'About') }
+    it { should have_selector('p', :text => 'Jake') }
+    it { should have_selector('title', :text => '| About') }
     end
-    it "should have the title content 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', 
-                              :text => '| About')
+
+  describe "Contact page" do
+    before { visit contact_path }
+      it { should have_selector('h1', :text => 'Contact') }
+      it { should have_selector('p', :text => 'San Francisco') }
+      it { should have_selector('title', :text => '| Contact') }
     end
-  end
+
+  describe "Help page" do
+    before { visit help_path }
+      it { should have_selector('h1', :text => 'Help') }
+      it { should have_selector('title', :text => '| Help') }
+    end
+
 end
